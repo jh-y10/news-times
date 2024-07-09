90d5859964362ce0a776e5bf785593630e94c81f
JunHyeok Yoon <jhy230912@gmail.com>
2024-07-09 18:12:38 +0900

    api fix

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const API_KEY = `51eb5f8f74ae4a6589f668b8e2aed355`;
+// const API_KEY = `51eb5f8f74ae4a6589f668b8e2aed355`;
 let news = [];
  const getLatestNews = async () => {
-  const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+  const url = new URL(`https://jhy-news-times.netlify.app/top-headlines`);
   console.log("uuu", url);
   const response = await fetch(url);
   console.log("rrr", response);
