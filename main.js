b388baf3c804de7cf04600a16d359c6935c3656f
JunHyeok Yoon <jhy230912@gmail.com>
2024-07-10 16:44:30 +0900

    news update

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 // const API_KEY = `51eb5f8f74ae4a6589f668b8e2aed355`;
-let news = [];
- const getLatestNews = async () => {
+let newsList = [];
+const getLatestNews = async () => {
   const url = new URL(`https://jhy-news-times.netlify.app/top-headlines`);
-  console.log("uuu", url);
   const response = await fetch(url);
-  console.log("rrr", response);
   const data = await response.json();
-  news = data.articles;
-  console.log("ddd",data); 
+  newsList = data.articles;
+  render();
+  console.log("dddd", newsList);
+};
+
+const render = () => {
+  const newsHTML = newsList.map(
+    (news) => `<div class="row news">
+          <div class="col-lg-4">
+            <img class="news-img-size" src=${news.urlToImage} alt="">
+          </div>
+          <div class="col-lg-8">
+            <h2>${news.title}</h2>
+            <p>${news.description}</p>
+            <div>
+              ${news.source.name} * ${news.publishedAt}
+            </div>
+          </div>
+        </div>`
+  ).join('');
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 getLatestNews();
