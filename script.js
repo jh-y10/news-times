c2ecc00905c6efbee042773e419f25be7620f75b
JunHyeok Yoon <jhy230912@gmail.com>
2025-02-26 03:45:05 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let news = [];
+const getLatestNews = async () => {
+  const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`);
+  console.log("uuu", url);
+  const response = await fetch(url);
+  const data = await response.json();
+  news = data.articles;
+  console.log("ddd", news);
+};
+
+getLatestNews();
